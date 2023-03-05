25f597b89a9e29e9ab6c277467b5d025d2438190
CripDepress <gritciaxx@gmail.com>
2023-03-05 16:34:05 +0700

    setting up files

---
+++ get_next_line.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akeawdou <akeawdou@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 15:21:23 by akeawdou          #+#    #+#             */
+/*   Updated: 2022/12/23 22:02:52 by akeawdou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char[FD_SETSIZE][BUFFER_SIZE]	stash;
+
+char	*get_next_line(int fd)
+{
+	char	*ret;
+	char	*buf;
+	int		r;
+	int		offset;
+
+	buf = (char *) malloc(sizeof (char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	offset = -1;
+	while (offset == -1)
+	{
+		r = read(fd, buf, BUFFER_SIZE);
+		if (r == -1)
+		{
+			free (buf);
+			return (NULL);
+		}
+		offset = ft_apndstr(ret, buf);
+	}
+	if (offset != 0)
+		ft_rember(stash, buf, offset);
+	return (ret);
+}
+
+int	ft_apndstr(char *dst, char *src)
+{
+	int	offset;
+
+	if (!dst)
+	{
+		dst = (char *) malloc(sizeof (char));
+		dst[0] = '\0';
+	}
+	offset = 0;
+	while (src[offset] != '\n')
+		offset++;
+
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akeawdou <akeawdou@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 15:13:31 by akeawdou          #+#    #+#             */
+/*   Updated: 2022/12/23 15:20:30 by akeawdou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# define BUFFER_SIZE 42
+
+char	*get_next_line.c(int fd);
+
+#endif
