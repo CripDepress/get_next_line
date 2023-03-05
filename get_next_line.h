5413e1ca1e19f98f668a8d18992556fb9f4d3c97
CripDepress <gritciaxx@gmail.com>
2023-03-05 22:04:45 +0700

    some changes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: akeawdou <akeawdou@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 15:13:31 by akeawdou          #+#    #+#             */
-/*   Updated: 2022/12/23 15:20:30 by akeawdou         ###   ########.fr       */
+/*   Updated: 2023/03/05 17:07:14 by akeawdou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <unistd.h>
 # define BUFFER_SIZE 42
 
-char	*get_next_line.c(int fd);
+char	*get_next_line(int fd);
 
 #endif
